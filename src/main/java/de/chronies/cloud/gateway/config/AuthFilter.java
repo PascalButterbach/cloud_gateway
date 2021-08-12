948212aafdad55526f612c557ff5d28521800f50
PascalButterbach <pascal@butterbach.org>
2021-08-12 21:20:20 +0200

    test

--- src/main/java/de/chronies/cloud/gateway/config/AuthFilter.java
+++ src/main/java/de/chronies/cloud/gateway/config/AuthFilter.java
@@ -3,9 +3,11 @@ package de.chronies.cloud.gateway.config;
 import de.chronies.cloud.gateway.dto.UserDto;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 
 @Component
@@ -20,9 +22,18 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
 
     @Override
     public GatewayFilter apply(Config config) {
+
+
         return (exchange, chain) -> {
+
+            //TEST
+            var buffer = exchange.getResponse().bufferFactory();
+
+
             if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-                throw new RuntimeException("Missing authentication information");
+                //TEST
+                return exchange.getResponse().writeWith(Flux.just(buffer.wrap("Missing authentication information".getBytes())));
+                //throw new RuntimeException("Missing authentication information");
             }
 
             String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
@@ -30,7 +41,9 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
             String[] parts = authHeader.split(" ");
 
             if (parts.length != 2 || !"Bearer".equals(parts[0])) {
-                throw new RuntimeException("Incorrect authentication structure");
+                //TEST
+                return exchange.getResponse().writeWith(Flux.just(buffer.wrap("Incorrect authentication structure".getBytes())));
+                //throw new RuntimeException("Incorrect authentication structure");
             }
 
             return webClientBuilder.build()
