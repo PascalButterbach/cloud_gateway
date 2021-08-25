f7f4680e8f3dcf90234cce6480d55bd5b4975809
PascalButterbach <pascal@butterbach.org>
2021-08-25 21:16:01 +0200

    fix

--- src/main/java/de/chronies/cloud/gateway/config/WebClientConfig.java
+++ src/main/java/de/chronies/cloud/gateway/config/WebClientConfig.java
@@ -3,38 +3,11 @@ package de.chronies.cloud.gateway.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
+
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
-
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowCredentials(true)
-//                .allowedOrigins("*")
-//                .allowedHeaders("*")
-//                .allowedMethods("*")
-//                .exposedHeaders("*");
-//    }
-
-//    @Bean
-//    public CorsWebFilter corsWebFilter() {
-//        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        corsConfiguration.setAllowCredentials(true);
-//        corsConfiguration.addAllowedHeader("*");
-//        corsConfiguration.addAllowedMethod("*");
-//        corsConfiguration.addAllowedOrigin("*");
-//        corsConfiguration.addExposedHeader("*");
-//        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
-//        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-//        return new CorsWebFilter(corsConfigurationSource);
-//    }
 
 
     @Bean
